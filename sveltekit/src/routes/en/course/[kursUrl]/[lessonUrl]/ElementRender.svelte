ec9c67a506777297271546b2291fd1b1b4b31498
Bruenner <bruenner@tugraz.at>
2026-02-15 20:59:51 +0100

    elementrenderfix

--- sveltekit/src/routes/en/course/[kursUrl]/[lessonUrl]/ElementRender.svelte
+++ sveltekit/src/routes/en/course/[kursUrl]/[lessonUrl]/ElementRender.svelte
@@ -112,7 +112,7 @@
         elementId: element.id
       };
       
-      const response = await fetch(resolve('/api/userProgress' , {
+      const response = await fetch(resolve('/api/userProgress') , {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
